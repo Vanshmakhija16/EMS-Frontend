45b6913a7860ef52156fb647137d104ac02fd4d9
Vanshmakhija16 <133850728+Vanshmakhija16@users.noreply.github.com>
2025-09-02 13:05:09 +0530

    Update Profile.jsx

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+const backend_url = import.meta.env.VITE_BACKEND_URL;
 
 const Home = () => {
   const [adminTotal, setAdminTotal] = useState(0)
