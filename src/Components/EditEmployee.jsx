f16a073f92a6322863e06d9fcf146c0913efc0c3
Vanshmakhija16 <133850728+Vanshmakhija16@users.noreply.github.com>
2025-09-02 13:03:45 +0530

    Update EditEmployee.jsx

--- src/Components/EditEmployee.jsx
+++ src/Components/EditEmployee.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+const backend_url = import.meta.env.VITE_BACKEND_URL;
 
 const EditEmployee = () => {
     const {id} = useParams()
